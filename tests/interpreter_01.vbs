860c2cbb169222a8021f0f01e2774b34186317a2
Louis Dubois <ldbo@protonmail.com>
2020-07-13 19:04:40 +0200

    new: Add simple literal expressions tests

---
+++ tests/interpreter_01.vbs
@@ -0,0 +1,5 @@
+' Evaluation of simple literal expressions
+1 + 2
+3 * 12 - 7
+3 * (12 - 7)
+1 + 2 + 3 + 4
---
+++ tests/test_interpreter.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+from emu import syntax, interpreter
+from tests.test import (assert_correct_function, run_function, SourceFile,
+                        Result)
+
+def interpreting(vbs: SourceFile) -> Result:
+    ast = syntax.parse_file(vbs)
+    pprint(ast.to_dict())
+    interp = interpreter.Interpreter()
+    return interp.evaluate_expression(ast.body[0])
+
+def evaluate_expressions(vbs: SourceFile) -> Result:
+    ast = syntax.parse_file(vbs)
+    interp = interpreter.Interpreter()
+    ret = []
+    for expression in ast.body:
+        ret.append(str(interp.evaluate_expression(expression)))
+    return ret
+
+def test_literal_expressions():
+    run_function('interpreter_01', evaluate_expressions)
+    assert(False)
