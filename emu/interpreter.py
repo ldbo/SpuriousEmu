acffac07cd2a6521f55c593bedfcf21275e0579f
Louis Dubois <ldbo@protonmail.com>
2020-07-22 16:32:26 +0200

    new: Add Interpreter.evaluate_function_call and solve resolution issue

--- emu/interpreter.py
+++ emu/interpreter.py
@@ -9,19 +9,21 @@ from .value import Value
 
 class Interpreter:
     _symbols: Symbol
+    _current_symbol: Symbol
     _memory: Dict[str, Function]
 
     def __init__(self, symbols=None, memory=None):
         self._symbols = symbols if symbols is not None else Symbol.build_root()
+        self._current_symbol = self._symbols
         self._memory = memory if memory is not None else Memory()
 
-    def evaluate_expression(self, expression):
+    def evaluate_expression(self, expression: Expression) -> Value:
         if isinstance(expression, Literal):
             return Value.from_literal(expression)
         elif isinstance(expression, Identifier):
             return self._memory.get_variable(expression.name)
         elif isinstance(expression, FunCall):
-            raise NotImplementedError("Don't call functions, you dumb ass")
+            return self.evaluate_function_call(expression)
         elif isinstance(expression, BinOp):
             left = self.evaluate_expression(expression.left)
             right = self.evaluate_expression(expression.right)
@@ -30,6 +32,24 @@ class Interpreter:
         elif isinstance(expression, UnOp):
             raise NotImplementedError(
                 "Don't use unary operators, you stupid s**t")
+
+    def evaluate_function_call(self, call: FunCall) -> Value:
+        # Resolution
+        function_symbol = self._current_symbol.resolve(call.function.name)
+        function_name = function_symbol.full_name()
+        function_object = self._memory.get_function(function_name)
+
+        # Arguments handling
+        arg_list = call.arguments.args
+        arg_values = list(map(self.evaluate_expression, arg_list))
+
+        # Move to the function
+        previous_symbol = self._current_symbol
+        self._current_symbol = function_symbol
+        return_value = self.call_function(function_object, arg_values)
+        self._current_symbol = previous_symbol
+
+        return return_value
 
     def execute_block(self, block: List[Statement]) -> None:
         for statement in block:
