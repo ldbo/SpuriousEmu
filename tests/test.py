088203907ee958666c707946b22110d1fb3edf31
Louis Dubois <ldbo@protonmail.com>
2020-08-10 15:09:55 +0200

    new: Add directory testing

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from pprint import pprint
 from typing import Callable, Dict, Any
 
@@ -22,18 +23,27 @@ def load_result(test: str) -> Result:
 def export_result(test: str, function: Callable[[SourceFile], Result]) -> None:
     """Export the result of the test to JSON"""
     with open(result_path(test), 'w') as f:
-        json.dump(function(vbs_path(test)), f, indent=4)
+        json.dump(function(source_path(test)), f, indent=4)
 
 
-def vbs_path(test: str) -> str:
-    """Return the path of the VBS source file of the test"""
-    return f"tests/source/{test}.vbs"
+def source_path(test: str) -> str:
+    """
+    Return the path of the VBS source file, or the project directory, of the
+    test.
+    """
+    root_path = f"tests/source/{test}"
+    if Path(root_path).is_dir():
+        return root_path
+    else:
+        path = root_path + ".vbs"
+        assert(Path(path).exists())
+        return path
 
 
 def assert_correct_function(test: str,
                             function: Callable[[SourceFile], Result]) -> None:
     """Load the expected result from JSON and compare to the function result"""
-    result = function(vbs_path(test))
+    result = function(source_path(test))
     expected_result = load_result(test)
     assert_equals(result, expected_result)
 
@@ -43,5 +53,5 @@ def run_function(test: str, function: Callable[[SourceFile], Result]) -> None:
     Display the result of a function call. Used during development until an
     expected result has been produced
     """
-    result = function(vbs_path(test))
+    result = function(source_path(test))
     pprint(result)
