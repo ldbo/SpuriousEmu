8a03fd0b0b7284bf3b9ac59d739ed85af1c4f393
Louis Dubois <ldbo@protonmail.com>
2020-08-17 12:45:21 +0200

    new: Add file creation

--- lib/WSH/Filesystem.py
+++ lib/WSH/Filesystem.py
@@ -5,3 +5,8 @@ class Filesystem:
     def FolderExists(interpreter, arguments):
         interpreter.add_file_event('FolderExists', arguments[0].value)
         return False
+
+    @staticmethod
+    def CreateTextFile(interpreter, arguments):
+        interpreter.add_file_event('CreateTextFile', arguments[0].value)
+        return interpreter.create_object('VBAEnv.WSH.FileHandler')
