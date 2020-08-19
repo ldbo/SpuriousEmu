7cf2ffeb06d7f636f12203887fe1c79c5c9319d6
Louis Dubois <ldbo@protonmail.com>
2020-08-19 15:54:43 +0200

    new: Implement Filesystem.CreateFolder

--- lib/WSH/Filesystem.py
+++ lib/WSH/Filesystem.py
@@ -9,7 +9,12 @@ class Filesystem:
     @staticmethod
     def CreateTextFile(interpreter, arguments):
         path = arguments[1].value
-        interpreter.add_file_event('CreateTextFile', arguments[1].value)
+        interpreter.add_file_event('CreateTextFile', path)
         file_handler = interpreter.create_object('VBAEnv.WSH.FileHandler')
         file_handler.value['path'] = path
         return file_handler
+
+    @staticmethod
+    def CreateFolder(interpreter, arguments):
+        path = arguments[1].value
+        interpreter.add_file_event('CreateFolder', path)
